c71dadbf43088f82eeedddc9842c0408f70b7644
mateusz <kucka.mateusz14@gmail.com>
2023-11-22 21:06:22 +0100

    ci.yaml

--- wena-tests/src/driver.ts
+++ wena-tests/src/driver.ts
@@ -5,7 +5,7 @@ let driver: WebDriver;
 
 const init = async () => {
     const options = new Options()
-    .addArguments('disable-dev-shm-usage', 'no-sandbox')
+    .addArguments('disable-dev-shm-usage', 'no-sandbox', 'headless')
     .setChromeBinaryPath('/opt/hostedtoolcache/chromium/latest/x64');
     driver = await new webdriver.Builder().forBrowser('chrome').setChromeOptions(options).build();
 }
