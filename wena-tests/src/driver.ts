6c5c9af735ea2b3ca9f82e6176687a4cbf07cb1e
mateusz <kucka.mateusz14@gmail.com>
2023-11-22 18:42:41 +0100

    ci.yaml

--- wena-tests/src/driver.ts
+++ wena-tests/src/driver.ts
@@ -4,7 +4,7 @@ import { Options } from "selenium-webdriver/chrome";
 let driver: WebDriver;
 
 const init = async () => {
-    const options = new Options().addArguments('--disable-dev-shm-usage');
+    const options = new Options().addArguments('disable-dev-shm-usage', 'no-sandbox');
     driver = await new webdriver.Builder().forBrowser('chrome').setChromeOptions(options).build();
 }
 
