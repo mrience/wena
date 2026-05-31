416a8beb4f655fe84b337bc7b1b3ee81a6f70479
Mateusz Kućka <kucka.mateusz14@gmail.com>
2026-05-31 13:50:57 +0200

    feat: Wena 17 update projects dependencies (#17)
    * feat: update projects dependencies
    
    * feat: update projects dependencies

--- wena-tests/src/driver.ts
+++ wena-tests/src/driver.ts
@@ -4,9 +4,9 @@ import { Options } from "selenium-webdriver/chrome";
 let driver: WebDriver;
 
 const init = async () => {
-    const options = new Options()
-    .addArguments('disable-dev-shm-usage', 'headless')
-    .setChromeBinaryPath('/opt/hostedtoolcache/chromium/latest/x64');
+    const options = new Options();
+    options.addArguments('disable-dev-shm-usage', 'headless');
+    options.setBinaryPath('/opt/hostedtoolcache/chromium/latest/x64');
     driver = await new webdriver.Builder().forBrowser('chrome').setChromeOptions(options).build();
 }
 
