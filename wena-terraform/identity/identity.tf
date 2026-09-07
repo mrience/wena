d18676a8352287e82d5e584b0aff28fb62505b1b
mateusz <kucka.mateusz14@gmail.com>
2026-09-07 20:35:24 +0200

    feat: create @Deploy move identity vars to variables.tf

--- wena-terraform/identity/identity.tf
+++ wena-terraform/identity/identity.tf
@@ -58,21 +58,6 @@ data "aws_iam_policy_document" "github_oidc_trust_policy" {
     }
 }
 
-variable "aws_account_id_dev" {
-  description = "Development AWS account ID to deploy resources in"
-  default     = "072055530432"
-}
-
-variable "aws_account_id_prod" {
-  description = "Production AWS account ID to deploy resources in"
-  default     = "558824711352"
-} 
-
-variable "aws_account_id_identity" {
-  description = "Identity AWS account ID to deploy resources in"
-  default     = "586808671648"
-}
-
 data "aws_iam_policy_document" "github_oidc_role_permissions" {
   statement {
     actions = ["sts:AssumeRole"]
