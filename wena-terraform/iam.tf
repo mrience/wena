3f1148a0f93644f95ec0a0da1a941b138985aa0f
mateusz <kucka.mateusz14@gmail.com>
2026-09-12 21:10:47 +0200

    temporary import @Deploy role to terraform state

--- wena-terraform/iam.tf
+++ wena-terraform/iam.tf
@@ -1,41 +1,52 @@
 data "aws_iam_policy_document" "deploy_role_trust_policy" {
-    statement {
-        effect = "Allow"
-        principals {
-            type = "AWS" 
-            identifiers = [ "arn:aws:iam::${var.aws_account_id_identity}:role/@GithubOidc" ]
-        }
-        actions = [ "sts:AssumeRole" ]
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${var.aws_account_id_identity}:role/@GithubOidc"]
     }
+    actions = ["sts:AssumeRole"]
+  }
 }
 
 data "aws_iam_policy_document" "deploy_role_permissions" {
-    statement {
-        actions   = ["iam:CreateRole", "iam:TagRole"]
-        resources = ["*"]
-    }
-
-    statement {
-        actions = [
-            "iam:GetRole",
-            "iam:AttachRolePolicy",
-            "iam:PutRolePolicy",
-            "iam:PassRole",
-            "iam:UpdateAssumeRolePolicy"
-        ]
-        resources = [ 
-            "arn:aws:iam::${var.aws_account_id}:role/*"
-        ]
-    }
+  statement {
+    actions = [
+      "iam:CreateRole",
+      "iam:TagRole",
+      "iam:GetRole",
+      "iam:GetRolePolicy",
+      "iam:AttachRolePolicy",
+      "iam:PutRolePolicy",
+      "iam:DeleteRolePolicy",
+      "iam:DeleteRole",
+      "iam:PassRole",
+      "iam:UpdateAssumeRolePolicy"
+    ]
+    resources = [
+      "arn:aws:iam::${var.aws_account_id}:role/*"
+    ]
+  }
 }
 
 resource "aws_iam_role" "deploy_role" {
-    name = "@Deploy"
-    assume_role_policy = data.aws_iam_policy_document.deploy_role_trust_policy.json
+  name               = "@Deploy"
+  assume_role_policy = data.aws_iam_policy_document.deploy_role_trust_policy.json
 }
 
 resource "aws_iam_role_policy" "deploy_role_policy_attachment" {
-    name = "deploy-role-policy-attachment"
-    role = aws_iam_role.deploy_role.id
-    policy = data.aws_iam_policy_document.deploy_role_permissions.json
+  name   = "deploy-role-policy-attachment"
+  role   = aws_iam_role.deploy_role.id
+  policy = data.aws_iam_policy_document.deploy_role_permissions.json
+}
+
+# Temporary: @Deploy was bootstrapped manually; import it instead of recreating. Remove after first apply on main.
+import {
+  to = aws_iam_role.deploy_role
+  id = "@Deploy"
+}
+
+import {
+  to = aws_iam_role_policy.deploy_role_policy_attachment
+  id = "@Deploy:deploy-role-policy-attachment"
 }
